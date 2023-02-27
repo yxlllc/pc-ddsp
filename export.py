56802b2894c71b80c15d55be8ad6426070c54290
yxlllc <33565655+yxlllc@users.noreply.github.com>
2023-02-27 23:20:34 +0800

    don't use wrapper for now

--- export.py
+++ export.py
@@ -54,7 +54,7 @@ def main():
 
     # load model
     model, args = load_model(cmd.model_path, device=device)
-    model = DDSPWrapper(model, device)
+    #model = DDSPWrapper(model, device)
 
     # extract model dirname and filename
     directory = os.path.dirname(os.path.abspath(cmd.model_path))
@@ -65,12 +65,13 @@ def main():
     n_frames = 10
     mel = torch.randn((1, n_frames, n_mel_channels), dtype=torch.float32, device=device)
     f0 = torch.FloatTensor([[440.] * n_frames]).to(device)
-
+    f0 = f0[..., None]
+    
     # export model
     with torch.no_grad():
         if cmd.traced:
             torch_version = torch.version.__version__.rsplit('+', maxsplit=1)[0]
-            export_path = os.path.join(directory, f'{name}-traced-torch{torch_version}.pt')
+            export_path = os.path.join(directory, f'{name}-traced-torch{torch_version}.jit')
             print(f' [Tracing] {cmd.model_path} => {export_path}')
             model = torch.jit.trace(
                 model,
